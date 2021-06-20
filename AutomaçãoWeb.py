7cd199778aa5d22ac10f708d944e0d825af95791
Mateus Leal <86207546+Mateus-L@users.noreply.github.com>
2021-06-20 19:32:35 -0300

    Add files via upload

---
+++ AutomaçãoWeb.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import pandas as pd
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Chrome()
+table = pd.read_csv('https://raw.githubusercontent.com/Mateus-L/ProjetosPy/main/Produtos.csv',sep=',',decimal='.')
+
+#pesquisar no google cotação dolar,euro e ouro
+driver.get('https://www.google.com.br/')
+
+#dolar
+driver.find_element_by_xpath('/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input').send_keys('Cotação Dolar')
+driver.find_element_by_xpath('/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input').send_keys(Keys.ENTER)
+dolar = driver.find_element_by_xpath('//*[@id="knowledge-currency__updatable-data-column"]/div[1]/div[2]/span[1]').get_attribute('data-value')
+#euro
+driver.find_element_by_xpath('//*[@id="tsf"]/div[1]/div[1]/div[2]/div/div[2]/input').send_keys(Keys.CONTROL+'A')
+driver.find_element_by_xpath('//*[@id="tsf"]/div[1]/div[1]/div[2]/div/div[2]/input').send_keys('Cotação Euro')
+driver.find_element_by_xpath('//*[@id="tsf"]/div[1]/div[1]/div[2]/div/div[2]/input').send_keys(Keys.ENTER)
+euro = driver.find_element_by_xpath('//*[@id="knowledge-currency__updatable-data-column"]/div[1]/div[2]/span[1]').get_attribute('data-value')
+#ouro
+driver.get('https://www.melhorcambio.com/ouro-hoje')
+ouro = driver.find_element_by_xpath('//*[@id="comercial"]').get_attribute('value')
+ouro = ouro.replace(',','.')
+
+#passo 1: atualizar as cotações e corrigir floats
+table.loc[table['Moeda']=='Dólar','Cotação'] = float(dolar)
+table.loc[table['Moeda']=='Euro','Cotação'] = float(euro)
+table.loc[table['Moeda']=='Ouro','Cotação'] = float(ouro)
+
+#passo 2: atualizar o preço base reais > cotação * preço base original
+table['Preço Base Reais'] = table['Cotação']*table['Preço Base Original']
+
+#passo 3: atualizar o preço final -> preço base reais * ajuste 
+table['Preço Final'] = table['Preço Base Reais']*table['Ajuste']
+table['Preço Final'] = table['Preço Final'].map('{:.2f}'.format)
+
+driver.quit()
+display(table)
+
